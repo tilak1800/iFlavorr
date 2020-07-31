036dc177895bfae4a2a53ff08ca59d1ac348ac9c
tilak1800 <69043460+tilak1800@users.noreply.github.com>
2020-07-31 09:25:51 -0700

    Created File

---
+++ iFlavourr/iFlavourr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iFlavourr
+//
+//  Created by Tilak Moradiya on 31/07/20.
+//  Copyright © 2020 iSoft System. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
